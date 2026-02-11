dee7d3184cb97e6fe3d4d87bbfbc0bb54093ec7c
AtIgnister <137562253+AtIgnister@users.noreply.github.com>
2026-02-11 10:52:36 +0100

    finish templating

--- stimmungsumfrage/src/main/java/org/lf10/stimmungsumfrage/Controllers/FeedbackFormController.java
+++ stimmungsumfrage/src/main/java/org/lf10/stimmungsumfrage/Controllers/FeedbackFormController.java
@@ -1,20 +1,30 @@
 package org.lf10.stimmungsumfrage.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/")
 public class FeedbackFormController {
-
     @GetMapping
-    public ModelAndView getTestData() {
+    public ModelAndView getForm() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("FeedbackForm");
         mv.getModel().put("data", "Welcome");
 
         return mv;
+    }
+
+    @PostMapping("/")
+    public String handleForm(
+        @RequestParam String feedback,
+        @RequestParam String mood, Model model
+    ) {
+        return "redirect:/";
     }
 }
