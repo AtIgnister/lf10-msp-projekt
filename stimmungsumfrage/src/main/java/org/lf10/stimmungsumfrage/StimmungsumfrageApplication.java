4005871a93c4d7cb28c2dc27b99f4ebda9caae17
AtIgnister <137562253+AtIgnister@users.noreply.github.com>
2026-02-11 09:36:12 +0100

    add project

---
+++ stimmungsumfrage/src/main/java/org/lf10/stimmungsumfrage/StimmungsumfrageApplication.java
@@ -0,0 +1,13 @@
+package org.lf10.stimmungsumfrage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StimmungsumfrageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StimmungsumfrageApplication.class, args);
+    }
+
+}
---
+++ stimmungsumfrage/src/test/java/org/lf10/stimmungsumfrage/StimmungsumfrageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lf10.stimmungsumfrage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StimmungsumfrageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
