9b823732693263f0d29ae0f4603ac36a6eaafda5
Topfpflanze5555 <Lars.Wellbrock@ewe.net>
2026-02-18 13:11:41 +0100

    SCRUM-43 Grundlegende HTML Javascript-Seite

---
+++ Webpage/main.js
@@ -0,0 +1,6 @@
+function copy()
+{
+    let selection = document.getElementById("emotion");
+    let comment = document.getElementById("comment");
+    navigator.clipboard.writeText("Emotion: " + selection.value + '; Comment: ' + comment.value + ';');
+}
