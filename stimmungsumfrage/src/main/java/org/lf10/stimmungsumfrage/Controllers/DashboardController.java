bbc3a452d83ebd28fdbcc9206dd7d562cc98450f
AtIgnister <137562253+AtIgnister@users.noreply.github.com>
2026-03-05 11:26:49 +0100

    add route

---
+++ stimmungsumfrage/src/main/java/org/lf10/stimmungsumfrage/Controllers/DashboardController.java
@@ -0,0 +1,16 @@
+package org.lf10.stimmungsumfrage.Controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
+@RequiredArgsConstructor
+public class DashboardController {
+    @GetMapping
+    public String index() {
+        return "dashboard";
+    }
+}
